1eb7cc9081ad413f177b99b545e6aee36802253f
alexandr.nesterenko <aleksandr.nesterenko@service-it.by>
2022-04-19 13:08:28 +0300

    refactors

--- Lenta/Screens/NewsList/NewsListViewController.swift
+++ Lenta/Screens/NewsList/NewsListViewController.swift
@@ -9,26 +9,26 @@ import UIKit
 
 final class NewsListViewController: UIViewController {
 
-//    fileprivate enum CellIdentifier: String {
-//        case newsList = "newsListCellIdentifier"
-//    }
-//
-//    private lazy var collectionView: UICollectionView = {
-//        let layout = NewsColumnListLayoutHelper.getNewsColumnListLayout(
-//            withInsets: .init(top: 16, leading: 16, bottom: 16, trailing: 16),
-//            interGroupScaping: 16
-//        )
-//        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.register(
-//            NewsItemCVCell.self,
-//            forCellWithReuseIdentifier: CellIdentifier.newsList.rawValue
-//        )
-//        view.delegate = self
-//        view.dataSource = self
-//        view.backgroundColor = Asset.Colors.white.color
-//        return view
-//    }()
+    fileprivate enum CellIdentifier: String {
+        case newsList = "newsListCellIdentifier"
+    }
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = NewsColumnListLayoutHelper.getNewsColumnListLayout(
+            withInsets: .init(top: 16, leading: 16, bottom: 16, trailing: 16),
+            interGroupScaping: 16
+        )
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(
+            NewsItemCVCell.self,
+            forCellWithReuseIdentifier: CellIdentifier.newsList.rawValue
+        )
+        view.delegate = self
+        view.dataSource = self
+        view.backgroundColor = Asset.Colors.white.color
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,45 +42,45 @@ final class NewsListViewController: UIViewController {
     private func setupView() {
         title = L10n.appTitle
         view.backgroundColor = Asset.Colors.white.color
-//
-//        view.addSubview(collectionView)
-//        let safeArea = view.safeAreaLayoutGuide
-//        NSLayoutConstraint.activate([
-//            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-//            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-//            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-//            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
-//        ])
+
+        view.addSubview(collectionView)
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
     
 }
-//
-//extension NewsListViewController: UICollectionViewDelegate {}
-//
-//// MARK: - UICollectionViewDataSource conformance
-//extension NewsListViewController: UICollectionViewDataSource {
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 4
-//    }
-//
-//    func collectionView(
-//        _ collectionView: UICollectionView,
-//        cellForItemAt indexPath: IndexPath
-//    ) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(
-//            withReuseIdentifier: CellIdentifier.newsList.rawValue,
-//            for: indexPath
-//        ) as! NewsItemCVCell
-//
-//
-//        cell.configure(
-//            title: "Hello Lenta",
-//            subtitle: "Hello Lenta\naskdlfkasd;lfka;sdlkf'als;dkf'als;dkf'as;dlkf'dl;f\n ;lk",
-//            image: nil
-//        )
-//
-//        return cell
-//    }
-//
-//}
+
+extension NewsListViewController: UICollectionViewDelegate {}
+
+// MARK: - UICollectionViewDataSource conformance
+extension NewsListViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: CellIdentifier.newsList.rawValue,
+            for: indexPath
+        ) as! NewsItemCVCell
+
+
+        cell.configure(
+            title: "Hello Lenta",
+            subtitle: "Hello Lenta\naskdlfkasd;lfka;sdlkf'als;dkf'als;dkf'as;dlkf'dl;f\n ;lk",
+            image: nil
+        )
+
+        return cell
+    }
+
+}
